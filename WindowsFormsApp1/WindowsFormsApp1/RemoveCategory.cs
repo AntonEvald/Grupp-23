26b8e7d547ed269a9a474ea6fbe59ac00efe902b
Anton Evald <anton.evald@gmail.com>
2017-10-18 21:47:27 +0200

    remove category tillagt

---
+++ WindowsFormsApp1/WindowsFormsApp1/RemoveCategory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace WindowsFormsApp1
+{
+    public partial class RemoveCategory : Form
+    {
+        public RemoveCategory()
+        {
+            InitializeComponent();
+            fillRemoveCombo();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void fillRemoveCombo()
+        {
+            List<string> list = ModifyCategories.Categories;
+            removeCombo.Items.Clear();
+            for (int i = 0; i < list.Count; i++)
+            {
+                string category = list[i];
+                removeCombo.Items.Add(category);
+            }
+        }
+    }
+}
