b468c39d0f9523a69b0013170766a125883477cc
AndreasDaun <Andreasdaun@hotmail.com>
2017-10-18 12:56:07 +0200

    added a connection to xml file to get podcast titles

--- WindowsFormsApp1/ClassLibrary1/Class1.cs
+++ WindowsFormsApp1/ClassLibrary1/Class1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary2;
 
 namespace ClassLibrary1
 {
+
     public class Class1
     {
+
     }
 }
---
+++ WindowsFormsApp1/ClassLibrary1/FetchXml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+
+namespace ClassLibrary1
+{
+    public class FetchXml
+    {
+
+        public static ClassLibrary2.FetchXml fetch = new ClassLibrary2.FetchXml();
+        public List<string> calledList = new List<string>();
+
+        public void PodcastList()
+        {
+            calledList = fetch.DownloadXmlTitles();
+        }
+
+    }
+}
