fe53c983ee26adba8c184b3cbbe498ec5faea75e
phille968 <philip96@hotmail.com>
2017-10-23 12:53:13 +0200

    oj?

--- WindowsFormsApp1/WindowsFormsApp1/AddFeed.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/AddFeed.Designer.cs
@@ -34,79 +34,96 @@
             this.AddFeedCategoryCB = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.AddCategoryBtn = new System.Windows.Forms.Button();
+            this.IntervallCb = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // urlTB
             // 
-            this.urlTB.Location = new System.Drawing.Point(32, 281);
-            this.urlTB.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.urlTB.Location = new System.Drawing.Point(12, 101);
             this.urlTB.Name = "urlTB";
-            this.urlTB.Size = new System.Drawing.Size(561, 38);
+            this.urlTB.Size = new System.Drawing.Size(213, 20);
             this.urlTB.TabIndex = 0;
             // 
             // urlLabel
             // 
             this.urlLabel.AutoSize = true;
-            this.urlLabel.Location = new System.Drawing.Point(35, 243);
-            this.urlLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.urlLabel.Location = new System.Drawing.Point(12, 85);
             this.urlLabel.Name = "urlLabel";
-            this.urlLabel.Size = new System.Drawing.Size(151, 32);
+            this.urlLabel.Size = new System.Drawing.Size(59, 13);
             this.urlLabel.TabIndex = 1;
             this.urlLabel.Text = "Feed URL:";
             // 
             // addFeedBtn
             // 
-            this.addFeedBtn.Location = new System.Drawing.Point(680, 281);
-            this.addFeedBtn.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.addFeedBtn.Location = new System.Drawing.Point(255, 152);
             this.addFeedBtn.Name = "addFeedBtn";
-            this.addFeedBtn.Size = new System.Drawing.Size(245, 55);
+            this.addFeedBtn.Size = new System.Drawing.Size(92, 23);
             this.addFeedBtn.TabIndex = 2;
             this.addFeedBtn.Text = "Add Feed";
             this.addFeedBtn.UseVisualStyleBackColor = true;
+            this.addFeedBtn.Click += new System.EventHandler(this.addFeedBtn_Click_1);
             // 
             // AddFeedCategoryCB
             // 
             this.AddFeedCategoryCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.AddFeedCategoryCB.FormattingEnabled = true;
-            this.AddFeedCategoryCB.Location = new System.Drawing.Point(32, 117);
-            this.AddFeedCategoryCB.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.AddFeedCategoryCB.Location = new System.Drawing.Point(12, 49);
             this.AddFeedCategoryCB.Name = "AddFeedCategoryCB";
-            this.AddFeedCategoryCB.Size = new System.Drawing.Size(316, 39);
-            this.AddFeedCategoryCB.TabIndex = 3;
+            this.AddFeedCategoryCB.Size = new System.Drawing.Size(121, 21);
+            this.AddFeedCategoryCB.TabIndex = 0;
             this.AddFeedCategoryCB.MouseClick += new System.Windows.Forms.MouseEventHandler(this.AddFeedCategoryCB_MouseClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 79);
-            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Location = new System.Drawing.Point(16, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(130, 32);
+            this.label1.Size = new System.Drawing.Size(49, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Category";
             // 
             // AddCategoryBtn
             // 
-            this.AddCategoryBtn.Location = new System.Drawing.Point(680, 101);
+            this.AddCategoryBtn.Location = new System.Drawing.Point(255, 42);
+            this.AddCategoryBtn.Margin = new System.Windows.Forms.Padding(1);
             this.AddCategoryBtn.Name = "AddCategoryBtn";
-            this.AddCategoryBtn.Size = new System.Drawing.Size(244, 55);
+            this.AddCategoryBtn.Size = new System.Drawing.Size(92, 23);
             this.AddCategoryBtn.TabIndex = 5;
             this.AddCategoryBtn.Text = "Add Category";
             this.AddCategoryBtn.UseVisualStyleBackColor = true;
             this.AddCategoryBtn.Click += new System.EventHandler(this.addCategoryBtn_Click);
             // 
+            // IntervallCb
+            // 
+            this.IntervallCb.FormattingEnabled = true;
+            this.IntervallCb.Location = new System.Drawing.Point(12, 152);
+            this.IntervallCb.Name = "IntervallCb";
+            this.IntervallCb.Size = new System.Drawing.Size(121, 21);
+            this.IntervallCb.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Intervall:";
+            // 
             // AddFeed
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1107, 551);
+            this.ClientSize = new System.Drawing.Size(415, 231);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.IntervallCb);
             this.Controls.Add(this.AddCategoryBtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.AddFeedCategoryCB);
             this.Controls.Add(this.addFeedBtn);
             this.Controls.Add(this.urlLabel);
             this.Controls.Add(this.urlTB);
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "AddFeed";
             this.Text = "AddFeed";
             this.ResumeLayout(false);
@@ -122,5 +139,7 @@
         private System.Windows.Forms.ComboBox AddFeedCategoryCB;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button AddCategoryBtn;
+        private System.Windows.Forms.ComboBox IntervallCb;
+        private System.Windows.Forms.Label label2;
     }
 }
