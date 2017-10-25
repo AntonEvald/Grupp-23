312cbf0d728250520f39da260864e9288da8b6a8
Philip Karlsson <philip96@hotmail.com>
2017-10-25 12:29:00 +0200

    TImer class början

---
+++ WindowsFormsApp1/ClassLibrary1/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ClassLibrary1
+{
+    class Timer
+    {
+        public Timer newTimer = new Timer();
+        String timerIN;
+
+        public Timer()
+        {
+            SetTimer(timerIN);
+        }
+         
+        public void SetTimer(string a)
+        {
+            newTimer.SetTimer(a);          
+
+        }
+    }
+}
