16f360fd6d9841db207f9959028bffc1a781b746
AndreasDaun <Andreasdaun@hotmail.com>
2017-10-26 12:13:25 +0200

    download mp3

---
+++ WindowsFormsApp1/WindowsFormsApp1/DownloadWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace WindowsFormsApp1
+{
+    public partial class DownloadWindow : Form
+    {
+        DownloadMp3 Download;
+
+        public DownloadWindow()
+        {
+            InitializeComponent();
+            Download = new DownloadMp3();
+            progressBar1.Value = Download.ProgressBar;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Download.Cancel();
+            this.Dispose();
+        }
+    }
+}
--- WindowsFormsApp1/WindowsFormsApp1/EpisodesForm.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/EpisodesForm.Designer.cs
@@ -73,12 +73,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(269, 208);
+            this.button1.Location = new System.Drawing.Point(310, 213);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "Download";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // EpisodesForm
             // 
