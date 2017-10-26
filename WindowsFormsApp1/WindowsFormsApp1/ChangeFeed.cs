dee3d13983ee7ccd718513674add70d196b90333
Anton Evald <anton.evald@gmail.com>
2017-10-26 11:42:18 +0200

    Ändringar i change feed

--- WindowsFormsApp1/WindowsFormsApp1/ChangeFeed.cs
+++ WindowsFormsApp1/WindowsFormsApp1/ChangeFeed.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClassLibrary1;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +16,56 @@ namespace WindowsFormsApp1
         public ChangeFeed()
         {
             InitializeComponent();
+            fillFeedCombo();
+            fillCatCombo();
+        }
+
+        private void fillFeedCombo()
+        {
+            PickFeedCombo.Items.Clear();
+            List<string> titles = PodcastNames.getPodcastsFromXML();
+            foreach (string title in titles)
+            {
+                PickFeedCombo.Items.Add(title);
+            }
+        }
+
+        private void fillCatCombo()
+        {
+            List<string> list = ModifyCategories.Categories;
+            foreach(string category in list)
+            {
+                changeCatCombo.Items.Add(category);
+            }
+        }
+
+        private void ChangeBtn_Click(object sender, EventArgs e)
+        {
+            string newCat = changeCatCombo.GetItemText(this.changeCatCombo.SelectedItem);
+            string feed = PickFeedCombo.GetItemText(this.PickFeedCombo.SelectedItem);
+            string newInt = intervalCombo.GetItemText(this.intervalCombo.SelectedItem);
+            if (Validation.textEmpty(feed))
+            {
+                MessageBox.Show("Du måste välja en feed att ändra!");
+            }
+            else
+            {
+                if (Validation.textEmpty(newCat))
+                {
+                    MessageBox.Show("Du måste välja en kategori!");
+                }
+                else
+                {
+                    if (Validation.textEmpty(newInt))
+                    {
+                        MessageBox.Show("Du måste välja en intervall");
+                    }
+                    else
+                    {
+                        XmlConection.UpdateXml(feed, newCat, newInt);
+                    }
+                }
+            }
         }
     }
 }
