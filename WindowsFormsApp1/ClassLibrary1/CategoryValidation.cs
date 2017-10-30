8bb3f7a227eb6eec50d4b52d2c0fe810288c3ff2
Anton Evald <anton.evald@gmail.com>
2017-10-30 14:04:08 +0100

    Fixat virtual

--- WindowsFormsApp1/ClassLibrary1/CategoryValidation.cs
+++ WindowsFormsApp1/ClassLibrary1/CategoryValidation.cs
@@ -8,7 +8,11 @@ namespace ClassLibrary1
 {
     public abstract class CategoryValidation
     {
-        public abstract bool IsNewCat(String c);
+        public virtual bool IsNewCat(String c)
+        {
+            c += c;
+            return true;
+        }
 
     }
 }
